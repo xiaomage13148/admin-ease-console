89d98e94400044f775403db9584400d4d4e33a12
xiaomage <15234829838@163.com>
2024-09-20 15:57:10 +0800

    fix: store配置

---
+++ src/enums/LanguageEnum.ts
@@ -0,0 +1,13 @@
+/**
+ * 语言枚举
+ */
+export const enum LanguageEnum {
+    /**
+     * 中文
+     */
+    ZH_CN = 'zh-cn',
+    /**
+     * 英文
+     */
+    EN = 'en'
+}
--- src/stores/modules/app.ts
+++ src/stores/modules/app.ts
@@ -1,5 +1,11 @@
 import {defineStore} from 'pinia';
+import {useStorage} from '@vueuse/core';
+import {LanguageEnum} from '@/enums/LanguageEnum';
 
 export const useAppStore = defineStore('app', () => {
+    // 语言
+    const language = useStorage('language', LanguageEnum.ZH_CN);
+
+    // 对应的语言包
 
 });
